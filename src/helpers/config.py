f956b8521b07b29349619f6fbd3ee5b4a09081ce
mostafa-3bdelaal <mostafa.ahmed.abdalal2005@gmail.com>
2026-09-23 04:59:47 +0300

    create interFace & OpenAIProvider

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     FILE_DEFAULT_CHUNK_SIZE: int
 
     MONGODB_URL: str
-    MONGODB_DATABASE: str
+    MONGODB_DATABASE: str   
 
     class Config:
         env_file = ".env"
